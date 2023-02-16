a2d1c6fad8fb452cb78123442d8ff8afed62ffa4
Niket Chawla <104648509+NC0312@users.noreply.github.com>
2023-02-16 10:45:14 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./main.scss";
+import "./footer.scss";
+import "./loadingwheel.scss";
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <Layout/>
+  );
+}
+
+export default App;
